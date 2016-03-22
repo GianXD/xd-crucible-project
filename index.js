c338c37d31f94c4e60cf22562af1e5ed108f1a05
GianXD <gian@xdresourcing.ca>
2016-03-21 21:58:55 -0400

    Added a postgres, plus test code

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 var http = require('http');
+var express = require('express');
+var app = express();
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
 
 var port = process.env.PORT || 8080;
 
